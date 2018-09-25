f75210af79d4ca4cc9c5b5c75541d619d76bac1d
Duret Nicolas <nico.duret@orange.fr>
2018-09-25 17:24:40 +0200

    Little example of GoogleSpeech API

---
+++ app/src/main/java/com/example/duret/lilia_alize_demo/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.duret.lilia_alize_demo;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.speech.RecognizerIntent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String recordResult = "";
+    private TextView resultAudioRecord;
+    private ImageButton recordButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultAudioRecord = findViewById(R.id.result_record);
+        recordButton = findViewById(R.id.recordButton);
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean canRecord = false;
+
+                if (checkPermission()) {
+                    canRecord = true;
+                }
+                else {
+                    requestPermission();
+                }
+
+                if (canRecord)
+                    recordAudio();
+            }
+        });
+
+    }
+
+    public void recordAudio() {
+        recordResult = "";
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Donnez moi un ordre :-)");
+        try {
+            startActivityForResult(intent, 100);
+        } catch (ActivityNotFoundException a) {
+            makeToast(a.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 100: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    recordResult = result.get(0).toLowerCase();
+                    resultAudioRecord.setText(recordResult);
+
+                }
+                break;
+            }
+        }
+    }
+
+    public boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    protected void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this, new
+                String[]{RECORD_AUDIO}, 1);
+    }
+
+    protected void makeToast(String text) {
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+    }
+}
