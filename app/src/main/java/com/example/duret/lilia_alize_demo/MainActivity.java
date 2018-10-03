072a192643ca0ec3bb80643cd6afd59c419c7d06
Duret Nicolas <nico.duret@orange.fr>
2018-10-03 21:21:15 +0200

    Set default settings on start

--- app/src/main/java/com/example/duret/lilia_alize_demo/MainActivity.java
+++ app/src/main/java/com/example/duret/lilia_alize_demo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.duret.lilia_alize_demo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -30,6 +32,12 @@ public class MainActivity extends BaseActivity {
                 startActivity(SettingsActivity.class);
             }
         });
+
+        if (Globals.getInstance().getIP() == null || Globals.getInstance().getPORT() == null) {
+            final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+            Globals.getInstance().setIP(sharedPref.getString("editIP_key", ""));
+            Globals.getInstance().setPORT(sharedPref.getString("editPORT_key", ""));
+        }
     }
 
     private View.OnClickListener startButtonListener = new View.OnClickListener() {
